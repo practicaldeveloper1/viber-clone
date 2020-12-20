2b1c32ba84f39aa2f889c5cd5fedd8690c00ee2d
practicaldeveloper1 <76068763+practicaldeveloper1@users.noreply.github.com>
2020-12-20 14:19:01 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
 import firebase from "firebase";
 
+// Please input your config
 const firebaseConfig = {
-  apiKey: "AIzaSyDIuPf67un02IoM9diFvujXT1g_gfgY6JE",
-  authDomain: "pd-viber-18c66.firebaseapp.com",
-  projectId: "pd-viber-18c66",
-  storageBucket: "pd-viber-18c66.appspot.com",
-  messagingSenderId: "745697906589",
-  appId: "1:745697906589:web:1edce1cea4aaa3f9d56974"
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
